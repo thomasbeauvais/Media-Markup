10643eb8d65ed474d18a67958c628a955657da16
thomasbeauvais <thomas.beauvais@hotmail.com>
2011-11-22 20:21:31 -0800

    Updated to convert

--- musicg-sound-api-custom/src/main/java/com/musicg/sound/test/Test.java
+++ musicg-sound-api-custom/src/main/java/com/musicg/sound/test/Test.java
@@ -1,24 +1,25 @@
 package com.musicg.sound.test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 import com.musicg.sound.Wave;
 import com.musicg.sound.WaveInputStream;
 import com.musicg.sound.graphic.GraphicRender;
 import com.musicg.sound.timedomain.AmplitudeTimeDomainRepresentation;
 import com.musicg.sound.timedomain.FrequencyTimeDomainRepresentation;
+import javazoom.jl.converter.Converter;
+import javazoom.jl.decoder.JavaLayerException;
 
 public class Test {
 
-    public static void main( String[] args ) {
+
+
+    public static void main( String[] args ) throws IOException {
 
         String name = "test";
 
-        String filename = name + ".wav";
-//        String filename = name + ".mp3";
+//        String filenameWav = name + ".wav";
+        String filenameMp3 = name + ".mp3";
 
         String strProjectBase   = "musicg-sound-api-custom/";
         String strProjectOutput = strProjectBase + "target/" + name + "/";
@@ -26,17 +27,19 @@ public class Test {
 
         new File( strProjectOutput ).mkdirs();
 
-        File file = new File( strProjectData, filename );
+        File fileMp3 = new File( strProjectData, filenameMp3 );
+        File fileWav = new File( "/tmp/test4743065779674894099.wav" );
+//        File fileWav = File.createTempFile( name, ".wav");
 
-        System.out.println( "Using inputfile: \t" + file.getAbsolutePath() );
+        System.out.println( "*** Using input file: \t" + fileMp3.getAbsolutePath() );
+
+//        convertMp3( fileMp3, fileWav ) ;
+
+        System.out.println( "*** Using input file: \t" + fileWav.getAbsolutePath() );
 
         // get the wave instance by input stream
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream( file );
-        } catch( FileNotFoundException e ) {
-            e.printStackTrace();
-        }
+//        InputStream fis = new FileInputStream( fileWav );
+        InputStream fis = new FileInputStream( fileWav );
 
         // create a wave inputStream by inputStream
         WaveInputStream wis = new WaveInputStream( fis );
@@ -49,22 +52,22 @@ public class Test {
 
         // TimeDomainRepresentations
         AmplitudeTimeDomainRepresentation ampRp = new AmplitudeTimeDomainRepresentation( wave );
-        FrequencyTimeDomainRepresentation freqRp = new FrequencyTimeDomainRepresentation( wave );
+//        FrequencyTimeDomainRepresentation freqRp = new FrequencyTimeDomainRepresentation( wave );
 
         // get the amplitude
-        double[] amplitudes = ampRp.getNormalizedAmplitudes();
+//        double[] amplitudes = ampRp.getNormalizedAmplitudes();
 
         // get the spectrogram
-        double[][] spectrogram = freqRp.getSpectrogram();
+//        double[][] spectrogram = freqRp.getSpectrogram();
 
         // Graphic render
         GraphicRender render = new GraphicRender();
-        render.renderWaveform( ampRp, strProjectOutput + "waveform.jpg" );
-        render.renderSpectrogram( freqRp, strProjectOutput + "spectrogram.jpg" );
+//        render.renderWaveform( ampRp, strProjectOutput + "waveform.jpg" );
+//        render.renderSpectrogram( freqRp, strProjectOutput + "spectrogram.jpg" );
 
 //        // change the amplitude representation
-//        ampRp.setTimeStep( 0.1F );
-//        render.renderWaveform( ampRp, strProjectOutput + "waveform2.jpg" );
+        ampRp.setTimeStep( 0.9F );
+        render.renderWaveform( ampRp, strProjectOutput + "waveform2.jpg" );
 //
 //        // change the spectrogram representation
 //        freqRp.setFftSampleSize( 512 );
@@ -77,5 +80,23 @@ public class Test {
 //
 //        // save the trimmed wav
 //        wave.saveAs( strProjectOutput + "out.wav" );
+    }
+
+    private static void convertMp3( File fileMp3, File fileOuputWav ) throws IOException {
+                InputStream fis = null;
+        try {
+            long start = System.currentTimeMillis();
+
+            System.out.println( "*** Converting file:   \t" + fileMp3.getAbsolutePath() );
+            System.out.println( "     to WAV file : \t" + fileOuputWav.getAbsolutePath() );
+
+            fis = new FileInputStream( fileMp3 );
+
+            new Converter().convert( fis , fileOuputWav.getAbsolutePath(), null, null );
+
+            System.out.println( "*** Conversion took " + (System.currentTimeMillis() - start ) + "ms");
+        } catch( JavaLayerException e ) {
+            throw new IOException( e );
+        }
     }
 }
