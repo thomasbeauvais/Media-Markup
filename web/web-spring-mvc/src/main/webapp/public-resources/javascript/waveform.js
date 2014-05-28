7904bb376f690ac99b32564cd5501a052d94f3ee
thomas.beauvais@hotmail.com <thomas.beauvais@hotmail.com>
2014-05-28 18:59:02 +0200

    Reformat

--- web/web-spring-mvc/src/main/webapp/public-resources/javascript/waveform.js
+++ web/web-spring-mvc/src/main/webapp/public-resources/javascript/waveform.js
@@ -1,12 +1,21 @@
-(function() {
+(function ()
+{
     var JSONP, Waveform,
-        __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+        __bind = function (fn, me)
+        {
+            return function ()
+            {
+                return fn.apply(me, arguments);
+            };
+        };
 
-    window.Waveform = Waveform = (function() {
+    window.Waveform = Waveform = (function ()
+    {
 
         Waveform.name = 'Waveform';
 
-        function Waveform(options) {
+        function Waveform(options)
+        {
             this.redraw = __bind(this.redraw, this);
             this.container = options.container;
             this.canvas = options.canvas;
@@ -14,13 +23,18 @@
             this.outerColor = options.outerColor || "transparent";
             this.innerColor = options.innerColor || "#000000";
             this.interpolate = true;
-            if (options.interpolate === false) {
+            if (options.interpolate === false)
+            {
                 this.interpolate = false;
             }
-            if (this.canvas == null) {
-                if (this.container) {
+            if (this.canvas == null)
+            {
+                if (this.container)
+                {
                     this.canvas = this.createCanvas(this.container, options.width || this.container.clientWidth, options.height || this.container.clientHeight);
-                } else {
+                }
+                else
+                {
                     throw "Either canvas or container option must be passed";
                 }
             }
@@ -28,51 +42,66 @@
             this.context = this.canvas.getContext("2d");
             this.width = parseInt(this.context.canvas.width, 10);
             this.height = parseInt(this.context.canvas.height, 10);
-            if (options.data) {
+            if (options.data)
+            {
                 this.update(options);
             }
         }
 
-        Waveform.prototype.setData = function(data) {
+        Waveform.prototype.setData = function (data)
+        {
             return this.data = data;
         };
 
-        Waveform.prototype.setDataInterpolated = function(data) {
+        Waveform.prototype.setDataInterpolated = function (data)
+        {
             return this.setData(this.interpolateArray(data, this.width));
         };
 
-        Waveform.prototype.setDataCropped = function(data) {
+        Waveform.prototype.setDataCropped = function (data)
+        {
             return this.setData(this.expandArray(data, this.width));
         };
 
-        Waveform.prototype.update = function(options) {
-            if (options.interpolate != null) {
+        Waveform.prototype.update = function (options)
+        {
+            if (options.interpolate != null)
+            {
                 this.interpolate = options.interpolate;
             }
-            if (this.interpolate === false) {
+            if (this.interpolate === false)
+            {
                 this.setDataCropped(options.data);
-            } else {
+            }
+            else
+            {
                 this.setDataInterpolated(options.data);
             }
             return this.redraw();
         };
 
-        Waveform.prototype.redraw = function() {
+        Waveform.prototype.redraw = function ()
+        {
             var d, i, middle, t, _i, _len, _ref, _results;
             this.clear();
-            if (typeof this.innerColor === "function") {
+            if (typeof this.innerColor === "function")
+            {
                 this.context.fillStyle = this.innerColor();
-            } else {
+            }
+            else
+            {
                 this.context.fillStyle = this.innerColor;
             }
             middle = this.height / 2;
             i = 0;
             _ref = this.data;
             _results = [];
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            for (_i = 0, _len = _ref.length; _i < _len; _i++)
+            {
                 d = _ref[_i];
                 t = this.width / this.data.length;
-                if (typeof this.innerColor === "function") {
+                if (typeof this.innerColor === "function")
+                {
                     this.context.fillStyle = this.innerColor(i / this.width, d);
                 }
                 this.context.clearRect(t * i, middle - middle * d, t, middle * d * 2);
@@ -82,18 +111,22 @@
             return _results;
         };
 
-        Waveform.prototype.clear = function() {
+        Waveform.prototype.clear = function ()
+        {
             this.context.fillStyle = this.outerColor;
             this.context.clearRect(0, 0, this.width, this.height);
             return this.context.fillRect(0, 0, this.width, this.height);
         };
 
-        Waveform.prototype.patchCanvasForIE = function(canvas) {
+        Waveform.prototype.patchCanvasForIE = function (canvas)
+        {
             var oldGetContext;
-            if (typeof window.G_vmlCanvasManager !== "undefined") {
+            if (typeof window.G_vmlCanvasManager !== "undefined")
+            {
                 canvas = window.G_vmlCanvasManager.initElement(canvas);
                 oldGetContext = canvas.getContext;
-                return canvas.getContext = function(a) {
+                return canvas.getContext = function (a)
+                {
                     var ctx;
                     ctx = oldGetContext.apply(canvas, arguments);
                     canvas.getContext = oldGetContext;
@@ -102,7 +135,8 @@
             }
         };
 
-        Waveform.prototype.createCanvas = function(container, width, height) {
+        Waveform.prototype.createCanvas = function (container, width, height)
+        {
             var canvas;
             canvas = document.createElement("canvas");
             container.appendChild(canvas);
@@ -111,33 +145,42 @@
             return canvas;
         };
 
-        Waveform.prototype.expandArray = function(data, limit, defaultValue) {
+        Waveform.prototype.expandArray = function (data, limit, defaultValue)
+        {
             var i, newData, _i, _ref;
-            if (defaultValue == null) {
+            if (defaultValue == null)
+            {
                 defaultValue = 0.0;
             }
             newData = [];
-            if (data.length > limit) {
+            if (data.length > limit)
+            {
                 newData = data.slice(data.length - limit, data.length);
-            } else {
-                for (i = _i = 0, _ref = limit - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+            }
+            else
+            {
+                for (i = _i = 0, _ref = limit - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i)
+                {
                     newData[i] = data[i] || defaultValue;
                 }
             }
             return newData;
         };
 
-        Waveform.prototype.linearInterpolate = function(before, after, atPoint) {
+        Waveform.prototype.linearInterpolate = function (before, after, atPoint)
+        {
             return before + (after - before) * atPoint;
         };
 
-        Waveform.prototype.interpolateArray = function(data, fitCount) {
+        Waveform.prototype.interpolateArray = function (data, fitCount)
+        {
             var after, atPoint, before, i, newData, springFactor, tmp;
             newData = new Array();
             springFactor = new Number((data.length - 1) / (fitCount - 1));
             newData[0] = data[0];
             i = 1;
-            while (i < fitCount - 1) {
+            while (i < fitCount - 1)
+            {
                 tmp = i * springFactor;
                 before = new Number(Math.floor(tmp)).toFixed();
                 after = new Number(Math.ceil(tmp)).toFixed();
@@ -149,25 +192,35 @@
             return newData;
         };
 
-        Waveform.prototype.optionsForSyncedStream = function(options) {
+        Waveform.prototype.optionsForSyncedStream = function (options)
+        {
             var innerColorWasSet, that;
-            if (options == null) {
+            if (options == null)
+            {
                 options = {};
             }
             innerColorWasSet = false;
             that = this;
             return {
                 whileplaying: this.redraw,
-                whileloading: function() {
+                whileloading: function ()
+                {
                     var stream;
-                    if (!innerColorWasSet) {
+                    if (!innerColorWasSet)
+                    {
                         stream = this;
-                        that.innerColor = function(x, y) {
-                            if (x < stream.position / stream.durationEstimate) {
+                        that.innerColor = function (x, y)
+                        {
+                            if (x < stream.position / stream.durationEstimate)
+                            {
                                 return options.playedColor || "rgba(255,  102, 0, 0.8)";
-                            } else if (x < stream.bytesLoaded / stream.bytesTotal) {
+                            }
+                            else if (x < stream.bytesLoaded / stream.bytesTotal)
+                            {
                                 return options.loadedColor || "rgba(0, 0, 0, 0.8)";
-                            } else {
+                            }
+                            else
+                            {
                                 return options.defaultColor || "rgba(0, 0, 0, 0.4)";
                             }
                         };
@@ -178,11 +231,13 @@
             };
         };
 
-        Waveform.prototype.dataFromSoundCloudTrack = function(track) {
+        Waveform.prototype.dataFromSoundCloudTrack = function (track)
+        {
             var _this = this;
             return JSONP.get("http://www.waveformjs.org/w", {
                 url: track.waveform_url
-            }, function(data) {
+            }, function (data)
+            {
                 return _this.update({
                     data: data
                 });
@@ -193,53 +248,69 @@
 
     })();
 
-    JSONP = (function() {
+    JSONP = (function ()
+    {
         var config, counter, encode, head, jsonp, key, load, query, setDefaults, window;
-        load = function(url) {
+        load = function (url)
+        {
             var done, head, script;
             script = document.createElement("script");
             done = false;
             script.src = url;
             script.async = true;
-            script.onload = script.onreadystatechange = function() {
-                if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+            script.onload = script.onreadystatechange = function ()
+            {
+                if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete"))
+                {
                     done = true;
                     script.onload = script.onreadystatechange = null;
-                    if (script && script.parentNode) {
+                    if (script && script.parentNode)
+                    {
                         return script.parentNode.removeChild(script);
                     }
                 }
             };
-            if (!head) {
+            if (!head)
+            {
                 head = document.getElementsByTagName("head")[0];
             }
             return head.appendChild(script);
         };
-        encode = function(str) {
+        encode = function (str)
+        {
             return encodeURIComponent(str);
         };
-        jsonp = function(url, params, callback, callbackName) {
+        jsonp = function (url, params, callback, callbackName)
+        {
             var key, query, uniqueName;
             query = ((url || "").indexOf("?") === -1 ? "?" : "&");
             callbackName = callbackName || config["callbackName"] || "callback";
             uniqueName = callbackName + "_json" + (++counter);
             params = params || {};
-            for (key in params) {
-                if (params.hasOwnProperty(key)) {
+            for (key in params)
+            {
+                if (params.hasOwnProperty(key))
+                {
                     query += encode(key) + "=" + encode(params[key]) + "&";
                 }
             }
-            window[uniqueName] = function(data) {
+            window[uniqueName] = function (data)
+            {
                 callback(data);
-                try {
+                try
+                {
                     delete window[uniqueName];
-                } catch (_error) {}
+                }
+                catch (_error)
+                {
+                }
                 return window[uniqueName] = null;
             };
             load(url + query + callbackName + "=" + uniqueName);
             return uniqueName;
         };
-        setDefaults = function(obj) {
+        setDefaults = function (obj)
+        {
             var config;
             return config = obj;
         };
